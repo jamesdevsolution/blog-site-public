f8bf30cb1bd665e59736e1b5e3946405765f3025
james talamo <jamestalamo4@gmail.com>
2026-04-03 23:58:48 +0800

    blog site

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,8 @@ const ArrowUpRight = () => (
 )
 
 const page = async () => {
+  const res = await fetch('https://api.visitorbadge.io/api/visitors?path=github.com%2Fblog-jamestalamo.com')
+
   return (
     <div className="bg-[#f0f2f4] dark:bg-[#151718] min-h-screen px-10  ">
       <div className="max-w-xl w-full mx-auto pt-16 flex flex-col gap-10">
